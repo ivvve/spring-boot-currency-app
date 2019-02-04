e4c9623b9977e0f7f1d5dd05358ff10bc2b39251
devson <sonyc5720@gmail.com>
2019-02-04 20:56:58 +0900

    프로젝트 생성

---
+++ src/test/java/me/devson/currency/CurrencyApplicationTests.java
@@ -0,0 +1,17 @@
+package me.devson.currency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
