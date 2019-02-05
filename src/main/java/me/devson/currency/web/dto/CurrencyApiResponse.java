b7c35df8dc35d25c5474e4d42f628dc24dad04da
devson <sonyc5720@gmail.com>
2019-02-05 12:39:07 +0900

    불필요한 필드 삭제

--- src/main/java/me/devson/currency/web/dto/CurrencyApiResponse.java
+++ src/main/java/me/devson/currency/web/dto/CurrencyApiResponse.java
@@ -9,8 +9,6 @@ import java.util.Map;
 @Setter
 public class CurrencyApiResponse {
     private boolean success;
-    private String terms;
-    private String privacy;
     private Long timestamp;
     private String source;
     private Map<String, String> quotes;
