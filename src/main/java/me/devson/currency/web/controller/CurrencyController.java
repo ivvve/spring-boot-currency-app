9ad521bcff58027782c0c487732484e189285798
devson <sonyc5720@gmail.com>
2019-02-05 16:34:09 +0900

    API 호출 : 캐시 사용하여 무분별한 API 호출을 막음

--- src/main/java/me/devson/currency/web/controller/CurrencyController.java
+++ src/main/java/me/devson/currency/web/controller/CurrencyController.java
@@ -2,13 +2,20 @@ package me.devson.currency.web.controller;
 
 import me.devson.currency.web.dto.CurrencyApiResponse;
 import me.devson.currency.web.service.CurrencyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@EnableCaching
 public class CurrencyController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyController.class);
+
     private final CurrencyService currencyService;
 
     public CurrencyController(CurrencyService currencyService) {
@@ -20,9 +27,12 @@ public class CurrencyController {
         return "currency";
     }
 
+    @Cacheable(cacheNames = "apiCache")
     @GetMapping("/currency")
     @ResponseBody
     public ResponseEntity<CurrencyApiResponse> getCurrency() {
+        LOGGER.debug("new cache was created");
+
         CurrencyApiResponse currencyApiResponse = currencyService.getCurrencyFromApi();
 
         if (!currencyApiResponse.isSuccess()) {
