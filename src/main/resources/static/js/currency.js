15f9fe5270c88960bfbec91e2e9e05277572fc42
devson <sonyc5720@gmail.com>
2019-02-05 13:54:28 +0900

    수취금액 가져오는 로직 추가

--- src/main/resources/static/js/currency.js
+++ src/main/resources/static/js/currency.js
@@ -4,26 +4,35 @@ window.onload = function() {
 
 const currency = {
     country: null,
-    remittance: null,
     exchangeRate: null,
     currency: null,
+    fineExchangeRate: null,
+    remittance: null,
+    remittanceAlertMessage: null,
+    amount: null,
 
     init() {
         this.country = document.getElementById("country");
-        this.remittance = document.getElementById("remittance");
         this.exchangeRate = document.getElementById("exchangeRate");
         this.currency = document.getElementById("currency");
+        this.fineExchangeRate = document.getElementById("fineExchangeRate");
+        this.remittance = document.getElementById("remittance");
+        this.remittanceAlertMessage = document.getElementById("remittanceAlertMessage");
+        this.amount = document.getElementById("amount");
 
         this.country.onchange();
     },
 
     async changeCurrency(country) {
         this.clearCurrency();
+        this.clearAmount();
 
         const currencyData = await this.getCurrency();
         let exchangeRate = currencyData.quotes["USD" + country];
-        exchangeRate = Number(exchangeRate).toFixed(2);
 
+        this.fineExchangeRate.value = exchangeRate;
+
+        exchangeRate = Number(exchangeRate).toFixed(2);
         this.exchangeRate.innerText = exchangeRate;
         this.currency.innerText = country + "/USD";
     },
@@ -33,8 +42,49 @@ const currency = {
             .then(response => response.json());
     },
 
+    getAmount() {
+        this.clearAmount();
+
+        this.remittance.value = this.remittance.value.trim();
+        let remittance = this.remittance.value;
+
+        if (!common.isNumber(remittance)) {
+            this.showRemittanceAlertMessage();
+            return;
+        }
+
+        remittance = Number(remittance);
+
+        if (!common.isPositive(remittance)) {
+            this.showRemittanceAlertMessage();
+            return;
+        }
+
+        const fineExchangeRate = Number(this.fineExchangeRate.value);
+        let amount = fineExchangeRate * remittance;
+        amount = amount.toFixed(2);
+        amount = Number(amount).toLocaleString();
+
+        this.showAmount(amount);
+    },
+
+    showAmount(amount) {
+        const country = this.country.value;
+        const amountText = `수취금액은 ${amount} ${country} 입니다.`;
+        this.amount.innerText = amountText;
+    },
+
     clearCurrency() {
         this.exchangeRate.innerText = "";
         this.currency.innerText = "";
+    },
+
+    clearAmount() {
+        this.remittanceAlertMessage.style.display = "none";
+        this.amount.innerText = "";
+    },
+
+    showRemittanceAlertMessage() {
+        this.remittanceAlertMessage.style.display = "inline";
     }
 };
